78f12c3d8b96658b0a7bb31176b376f58d298e2f
Denis Gavrilin <den.gavrilin1@gmail.com>
2020-06-08 20:24:39 +0300

    fixed admin

--- app/helpers/spree/admin/navigation_helper_decorator.rb
+++ app/helpers/spree/admin/navigation_helper_decorator.rb
@@ -1,13 +1,7 @@
-module Spree
-  module Admin
-    module NavigationHelperDecorator
-      def link_to_blog_in_store(resource, options = {})
-        url = options[:url] || blog_entry_permalink(resource)
-        #options[:data] = {:action => 'show'}
-        link_to(Spree.t(:view_in_store), url, options)
-      end
-    end
+Spree::Admin::NavigationHelper.module_eval do
+  def link_to_blog_in_store(resource, options={})
+    url = options[:url] || blog_entry_permalink(resource)
+    #options[:data] = {:action => 'show'}
+    link_to(Spree.t(:view_in_store), url, options)
   end
 end
-
-::Spree::Admin::NavigationHelper.prepend(Spree::Admin::NavigationHelperDecorator)
--- app/overrides/add_blog_admin_menu_tab.rb
+++ app/overrides/add_blog_admin_menu_tab.rb
@@ -1,5 +1,5 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
-                     :name => "blog_admin_tabs",
-                     :insert_bottom => "#main-sidebar",
-                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar'><%= tab Spree.t(:'admin.tab.blog_entries'), url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
+Deface::Override.new(virtual_path: 'spree/admin/shared/_main_menu',
+                     name: 'pages_admin_sidebar_menu',
+                     insert_bottom: 'nav',
+                     :text => "<% if Spree.user_class && can?(:admin, Spree::BlogEntry) %><ul class='nav nav-sidebar border-bottom'><%= tab Spree.t(:'admin.tab.blog_entries'), url: spree.admin_blog_entries_path, icon: 'comment' %></ul><% end %>",
                      :disabled => false)
